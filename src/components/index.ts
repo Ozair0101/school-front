76fac05e003d5e9b62a371a8dd5602631fb4f96f
Ozair0101 <ozairjan186@gmail.com>
2025-10-19 15:50:33 +0430

    index

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as ExamCard } from './ExamCard';
+export { default as ScoreBadge } from './ScoreBadge';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Header } from './Header';
+export { default as ConfettiEffect } from './ConfettiEffect';
