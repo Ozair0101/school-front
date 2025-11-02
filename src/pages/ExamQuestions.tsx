dec0645491f36d491829ddeaf793747ed7007764
Ozair0101 <ozairjan186@gmail.com>
2025-11-02 15:40:54 +0430

    exa

--- src/pages/ExamQuestions.tsx
+++ src/pages/ExamQuestions.tsx
@@ -259,7 +259,7 @@ const ExamQuestions: React.FC = () => {
                 Total Marks
               </div>
               <div className="text-2xl font-bold text-gray-900 dark:text-white break-words">
-                {examQuestions.reduce((sum, eq) => sum + (eq.marks || eq.question?.default_marks || 0), 0).toFixed(2)}
+                {examQuestions.reduce((sum, eq) => sum + Number(eq.marks || eq.question?.default_marks || 0), 0).toFixed(2)}
               </div>
             </Card>
             <Card>
